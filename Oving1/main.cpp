82f649426e668fdac44dd9edf07f7e45ba6bbc8e
Bragegs <bragestaven94@hotmail.com>
2016-08-16 22:21:33 +0200

    Oving1_A

---
+++ Oving1/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+  
+  const int antall = 5;
+  int antall_u_10 = 0;
+  int antall_m_10_20 = 0;
+  int antall_o_20 = 0;
+  int temp;
+  
+  for(int i = 0; i < antall; i++){
+    std::cout << "Temperatur for dag " << (i+1) << ": " << std::endl;
+    std::cin >> temp;
+   // std::cout << temp << std::endl;
+    
+    if (temp < 10) {
+      antall_u_10++;
+    } else if (temp > 10 && temp < 20) {
+      antall_m_10_20++;
+    } else {
+      antall_o_20++;
+    }
+    
+    if ( (i+1) == antall ) {
+      std::cout << "Antall under 10: " << antall_u_10 << std::endl << "Antall mellom 10 og 20: " << antall_m_10_20 << std::endl << "Antallet over 20: " << antall_o_20 << std::endl;
+    }
+  }
+  
+}
