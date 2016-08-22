3793a68f0ea0b009285ebc653ef8f2d2cf01b2b7
Bragegs <bragestaven94@hotmail.com>
2016-08-22 16:34:46 +0200

    project structure

---
+++ Oving1/1A/build/CMakeFiles/1A.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1A.dir/Oving1A.cpp.o"
+  "1A.pdb"
+  "1A"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Oving1/1B/build/CMakeFiles/1B.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1B.dir/Oving1B.cpp.o"
+  "1B.pdb"
+  "1B"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
